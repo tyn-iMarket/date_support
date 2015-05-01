c90dfecef47574c6da5bb2008559c7c619eb066f
Takehiko Shinkura <tynmarket@gmail.com>
2015-05-01 15:00:24 +0900

    Add day_to method

--- lib/date_support.rb
+++ lib/date_support.rb
@@ -1,5 +1,14 @@
 require "date_support/version"
 
-module DateSupport
-  # Your code goes here...
+class Date
+
+  def day_to(day)
+    cwday = sunday? ? 0 : cwday()
+    cwdays = {sunday: 0, monday: 1, tuesday: 2, wednesday: 3,
+      thursday: 4, friday: 5, saturday: 6}
+    adder = cwdays[day] - cwday
+
+    self + adder
+  end
+
 end
--- spec/date_support_spec.rb
+++ spec/date_support_spec.rb
@@ -1,11 +1,53 @@
 require 'spec_helper'
 
-describe DateSupport do
-  it 'has a version number' do
-    expect(DateSupport::VERSION).not_to be nil
+describe Date do
+
+  describe 'day_to' do
+    let(:day_to) { date.day_to day }
+
+    context 'date is 2015/5/3 (sunday)' do
+      let(:date) { Date.new 2015, 5, 3 }
+
+      context 'day to sunday' do
+        let(:day) { :sunday }
+
+        it { expect(day_to).to eq Date.new(2015, 5, 3) }
+      end
+
+      context 'day to wednesday' do
+        let(:day) { :wednesday }
+
+        it { expect(day_to).to eq Date.new(2015, 5, 6) }
+      end
+
+      context 'day to saturday' do
+        let(:day) { :saturday }
+
+        it { expect(day_to).to eq Date.new(2015, 5, 9) }
+      end
+    end
+
+    context 'date is 2015/5/9 (saturday)' do
+      let(:date) { Date.new 2015, 5, 9 }
+
+      context 'day to sunday' do
+        let(:day) { :sunday }
+
+        it { expect(day_to).to eq Date.new(2015, 5, 3) }
+      end
+
+      context 'day to wednesday' do
+        let(:day) { :wednesday }
+
+        it { expect(day_to).to eq Date.new(2015, 5, 6) }
+      end
+
+      context 'day to saturday' do
+        let(:day) { :saturday }
+
+        it { expect(day_to).to eq Date.new(2015, 5, 9) }
+      end
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
