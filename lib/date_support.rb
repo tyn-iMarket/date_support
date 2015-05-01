166d533da4d8a2464277e7c5b07e962217c35feb
Takehiko Shinkura <tynmarket@gmail.com>
2015-05-01 20:40:44 +0900

    Change cwday to wday

--- lib/date_support.rb
+++ lib/date_support.rb
@@ -3,10 +3,9 @@ require "date_support/version"
 class Date
 
   def day_to(day)
-    cwday = sunday? ? 0 : cwday()
-    cwdays = {sunday: 0, monday: 1, tuesday: 2, wednesday: 3,
+    wdays = {sunday: 0, monday: 1, tuesday: 2, wednesday: 3,
       thursday: 4, friday: 5, saturday: 6}
-    adder = cwdays[day] - cwday
+    adder = wdays[day] - wday
 
     self + adder
   end
